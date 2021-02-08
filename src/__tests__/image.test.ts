3b3ec339b35dac9a18a2144a5576148f0a44f04d
hancel.lin <imlinhanchao@foxmail.com>
2021-02-08 13:46:36 +0800

    update git repository

--- src/__tests__/image.test.ts
+++ src/__tests__/image.test.ts
@@ -2,13 +2,13 @@ import img from '../index';
 import * as path from 'path';
 
 test('Check Image File', () => {
-    expect(img.isImage(path.join(__dirname, 'test'), ['png'])).toBe(true);
+  expect(img.isImage(path.join(__dirname, 'test'), ['png'])).toBe(true);
 });
 
 test('Get Image Suffix', () => {
-    expect(img.getImageSuffix(path.join(__dirname, 'test'), img.suffixs)).toBe('png');
+  expect(img.getImageSuffix(path.join(__dirname, 'test'), img.suffixs)).toBe('png');
 });
 
 test('Check Specified Type Image', () => {
-    expect(img.isImage(path.join(__dirname, 'test'), ['jpg'])).toBe(false);
+  expect(img.isImage(path.join(__dirname, 'test'), ['jpg'])).toBe(false);
 });
